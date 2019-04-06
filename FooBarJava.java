3dc29f3a3321e05ac3058fc828697665187cb5b1
glorsh66 <30538582+glorsh66@users.noreply.github.com>
2019-04-06 12:28:06 +0300

    first commit

---
+++ FooBarJava.java
@@ -0,0 +1,23 @@
+public class   {
+//    Write a program that prints the numbers from 1 to 100.
+//    But for multiples of three print “Foo” instead of the number
+//    and for the multiples of five print “Bar”.
+//    For numbers which are multiples of both three and five print “FooBar”.
+
+
+    public static void FooBar(int f, int to)
+    {
+        for (int i = f; i <= to; i++)
+        {
+            if ( (i % 5) == 0 && (i%3) == 0) System.out.println("FooBar");
+            else if ((i % 5) == 0) System.out.println("Bar");
+            else if ((i % 3) == 0) System.out.println("Foo");
+            else System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        FooBar(1,100);
+    }
+
+}
